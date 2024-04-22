b961647b6133e34f3da408c7f7312c197d392cfc
GuilhermeGiovanucci <guigiovanucci@hotmail.com>
2024-04-22 18:35:23 -0300

    Exercício 5
    Inclusão de tipos primitivos

---
+++ Exercício 4.py
@@ -0,0 +1,5 @@
+n1 = int(input ('Número 1: '))
+n2 = int(input ('Número 2: '))
+s = n1 + n2
+#print ('A soma entre',(n1),'e',(n2),'é',(s))
+print('A soma entre {} e {} é {}'.format(n1,n2,s))
---
+++ Exercício 5.py
@@ -0,0 +1,2 @@
+n1 =bool(input ('Digite algo: '))
+print = (type,(n1))
